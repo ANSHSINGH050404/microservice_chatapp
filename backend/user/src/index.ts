e3c22fcfa0e451f5c04e400bd49c6492f5f2cb21
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2025-09-04 21:14:16 +0530

    initial commit

---
+++ backend/user/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
+import userRouter from './routes/user.js'
+import { connectRabbitMQ } from "./config/rabbitmq.js";
+dotenv.config();
+
+connectDb();
+
+connectRabbitMQ();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("Connected to redis"))
+  .catch(console.error);
+const app = express();
+
+
+app.use("api/v1",userRouter)
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
